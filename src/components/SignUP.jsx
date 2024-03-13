e439975b91d6d71e20fe6756bca2b62762eff544
abhijeetkhokhar <abhijeetkhokhar01@gmail.com>
2024-03-13 17:21:23 +0530

    update

--- src/components/SignUP.jsx
+++ src/components/SignUP.jsx
@@ -28,16 +28,16 @@ export default function SignUP({userdata}) {
     <div className={styles.signup}>
         <h1>SignUp form</h1>
        <form onSubmit={handleSubmit}>
-        <label htmlFor="user">UserName</label>
-        <input type="text" id='user' value={name} onChange={handleChange} /> <br />
+        <label htmlFor="user"></label>
+        <input type="text" id='user' value={name} onChange={handleChange} placeholder='username'/> <br />
         <br />
-        <label htmlFor="pass">Password</label>
-        <input type="password" id='pass' value={pass} onChange={handleChange}/> <br />
+        <label htmlFor="pass"></label>
+        <input type="password" id='pass' value={pass} onChange={handleChange} placeholder='password'/> <br />
         <br />
-        <label htmlFor="phone">PhoneNo:</label>
-        <input type="text" id="phone"  value={phone} onChange={handleChange}/> <br /> <br />
-        <label htmlFor="emailId">EmailId:</label>
-        <input type="email"  id="emailId"  value={email} onChange={handleChange}/>
+        <label htmlFor="phone"></label>
+        <input type="text" id="phone"  value={phone} onChange={handleChange} placeholder='phone number'/> <br /> <br />
+        <label htmlFor="emailId"></label>
+        <input type="email"  id="emailId"  value={email} onChange={handleChange} placeholder='email'/>
         <br /> <br />
         <button>Login</button>
        </form>
